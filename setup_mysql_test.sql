05fb970e9745855f335bf2de6c315d252b78329b
joseph thuku <jthuku490@gmail.com>
2023-03-22 12:57:52 +0000

    setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,9 @@
--- script to create database hbnb_test_db
---- A new user hbnb_test (in localhost)
-CREATE DATABASE IF NOT EXIST hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED AS 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbn_tets'@'localhost'
+-- This script creates a database: hbnb_test_db
+-- a new user hbnb_test in localhost
+-- grants all privileges to the user on the hbnb_test_db database
+-- grants SELECT privelege on the performance_schema database
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
