620bc7aca57d7a8027b8fef25dc2362125af2714
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-03-22 22:33:32 +0300

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,19 @@
--- This script creates a database: hbnb_test_db
--- a new user hbnb_test in localhost
--- grants all privileges to the user on the hbnb_test_db database
--- grants SELECT privelege on the performance_schema database
+-- Creates a MySQL server with:
+--   Database hbnb_test_db.
+--   User hbnb_test with password hbnb_test_pwd in localhost.
+--   Grants all privileges for hbnb_test on hbnb_test_db.
+--   Grants SELECT privilege for hbnb_test on performance_schema.
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+CREATE USER
+    IF NOT EXISTS 'hbnb_test'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_test_db`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;
