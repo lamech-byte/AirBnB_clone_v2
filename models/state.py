40ad9bcd5c5cad8af8b896af78b4d8dc6287451b
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-03-23 15:52:48 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""Defines the State class."""
+import models
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
 from models.city import City
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-import os
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """Represents a state for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
+    """
     __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
 
-    if os.environ.get('HBNB_TYPE_STORAGE') == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship("City",
-                              backref="states",
-                              cascade="all, delete, delete-orphan")
-
-    else:
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """ Returns the list of City instances with
-            state_id == current State.id """
-            from models import storage
-            tmp_cities = []
-            all_cities = storage.all(City)
-            for city in all_cities.values():
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
                 if city.state_id == self.id:
-                    tmp_cities.append(city)
-            return tmp_cities
+                    city_list.append(city)
+            return city_list
