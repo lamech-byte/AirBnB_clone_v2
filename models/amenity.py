7bedfb98f5996e9bbac94c6801e748edec9a6f23
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-03-24 09:47:24 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+"""
+Amenity Class from Models Module
+"""
+import os
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database.
+    """Amenity class handles all application amenities"""
 
-    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
+    if os.getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place",
+                                       secondary="place_amenity",
+                                       viewonly=False)
+    else:
+        name = ""
 
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Amenities.
-        name (sqlalchemy String): The amenity name.
-        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    def __init__(self, *args, **kwargs):
+        """instantiates a new amenity"""
+        super().__init__(self, *args, **kwargs)
