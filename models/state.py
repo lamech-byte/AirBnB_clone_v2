65301b34c13b75d181b7c4cca641f644ac31aa0a
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-03-24 09:34:28 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from os import getenv
-from sqlalchemy import String, DateTime, Column, ForeignKey
-from sqlalchemy.orm import relationship
-import models
+"""
+State Class from Models Module
+"""
+
 from models.base_model import BaseModel, Base
-from models.city import City
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+import os
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+    """State class handles all application states"""
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
+    if os.getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade="all, delete", backref='state')
     else:
-        @property
-        def cities(self):
-            """Getter attribute in case of file storage"""
-            return [city for city in models.storage.all(City).values()
-                    if city.state_id == self.id]
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """instantiates a new state"""
+        super().__init__(self, *args, **kwargs)
+
+    @property
+    def cities(self):
+        """return list of cities"""
+        if os.getenv('HBNB_TYPE_STORAGE', '') != 'db':
+            all_cities = models.storage.all("City")
+            city_list = []
+            for cityid in all_cities:
+                if all_cities[cityid].state_id == self.id:
+                    city_list.append(all_cities[cityid])
+            return city_list
