09c57d1beedb08e088581afd52369ab52e0eaf3e
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-03-24 11:50:24 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-import os
-from models.engine import file_storage
-from models.engine import db_storage
-from models.base_model import BaseModel, Base
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place, PlaceAmenity
-from models.review import Review
-from models.state import State
-from models.user import User
-from sqlalchemy import MetaData
+"""Instantiates a storage object.
 
-CNC = file_storage.FileStorage.CNC
-DNC = db_storage.DBStorage.DNC
-if os.getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
-    storage = db_storage.DBStorage()
-    storage.reload()
+-> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
+   instantiates a database storage engine (DBStorage).
+-> Otherwise, instantiates a file storage engine (FileStorage).
+"""
+from os import getenv
+
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
 else:
-    storage = file_storage.FileStorage()
-    storage.reload()
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+storage.reload()
