7e3b2199b1de27b49ff764656d829bdda8c2fbbf
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-03-24 09:33:34 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-"""This is the city class"""
-from sqlalchemy import String, DateTime, Column, ForeignKey
-from sqlalchemy.orm import relationship
+"""
+City Class from Models Module
+"""
+
 from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Table
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+import os
 
 
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', backref='cities',
-                          cascade='all, delete-orphan')
+    """City class handles all application cities"""
+
+    if os.getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', cascade="all, delete", backref="cities")
+    else:
+        name = ""
+        state_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """instantiates a new city"""
+        super().__init__(self, *args, **kwargs)
