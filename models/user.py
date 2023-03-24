3cc73a27f0ab190747dd4b39975c20f5e0f2aa39
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-03-24 12:11:12 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-"""
-User Class from Models Module
-"""
-
-from models.base_model import BaseModel, Base
-import sqlalchemy
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+"""Defines the User class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-import os
 
 
 class User(BaseModel, Base):
-    """User class handles all application users"""
+    """Represents a user for a MySQL database.
 
-    if os.getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-        places = relationship('Place', cascade="all, delete", backref='user')
-        reviews = relationship('Review', cascade="all, delete", backref='user')
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    Inherits from SQLAlchemy Base and links to the MySQL table users.
 
-    def __init__(self, *args, **kwargs):
-        """instantiates a new user"""
-        super().__init__(self, *args, **kwargs)
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store users.
+        email: (sqlalchemy String): The user's email address.
+        password (sqlalchemy String): The user's password.
+        first_name (sqlalchemy String): The user's first name.
+        last_name (sqlalchemy String): The user's last name.
+        places (sqlalchemy relationship): The User-Place relationship.
+        reviews (sqlalchemy relationship): The User-Review relationship.
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
