7bfec7e779a4dc7533e1e088af55961224c88f4d
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-06 16:52:58 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,46 +2,57 @@
 # Fabric script that distributes an archive to web servers
 # distributes an archive to your web servers, using the function do_deploy:
 
-from fabric.api import *
-from os.path import exists
+from fabric.api import run, put, env, local
 import os
+from datetime import datetime
 
 env.hosts = ['54.236.17.124', '100.24.237.123']
 
 # Update with your username
-env.user = "ubuntu"  
+env.user = "ubuntu"
 
-# Update with your private key path
-env.key_filename = '~/.ssh/id_rsa'  
 
+def do_pack():
+    """
+    Function to create a .tgz archive.
+    """
+    try:
+        local("mkdir -p versions")
+        d_t = datetime.now().strftime("%Y%m%d%H%M%S")
+        compr_file = "versions/web_static_{}.tgz".format(d_t)
+        local("tar -cvzf {} web_static".format(compr_file))
+        return compr_file
+    except:
+        return None
 
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers"""
-    if not os.path.exists(archive_path):
-        return False
-
-    try:
+    """
+    Function that distributes an archive.
+    """
+    name_dir = archive_path[9:-4]
+    if os.path.exists(archive_path):
+        
         # Upload archive to /tmp/ directory of web server
-        put(archive_path, "/tmp/")
-
+        put(archive_path, '/tmp/')
+        
         # Uncompress archive to /data/web_static/releases/
-        archive_filename = os.path.basename(archive_path)
-        archive_no_ext = os.path.splitext(archive_filename)[0]
-        run("mkdir -p /data/web_static/releases/{}".format(archive_no_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(archive_filename, archive_no_ext))
-        run("rm /tmp/{}".format(archive_filename))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/"
-            .format(archive_no_ext, archive_no_ext))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(archive_no_ext))
-
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(name_dir))
+        run("sudo tar -xzf /tmp/{}.tgz -C \
+        /data/web_static/releases/{}/".format(name_dir, name_dir))
+        run("sudo rm /tmp/{}.tgz".format(name_dir))
+        run("sudo mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".
+            format(name_dir, name_dir))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static"
+            .format(name_dir))
+        
         # Delete symbolic link
-        run("rm -rf /data/web_static/current")
-
+        run("sudo rm -rf /data/web_static/current")
+        
         # Create new symbolic link
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(archive_no_ext))
+        run("sudo ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current".format(name_dir))
+        print("New version deployed!")
         return True
-
-    except:
+    else:
         return False
