cb6b8fda8efd136f2f8f2bcf69850c41bc3c4256
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-06 17:53:02 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,58 +1,61 @@
 #!/usr/bin/python3
-# Fabric script that distributes an archive to web servers
-# distributes an archive to your web servers, using the function do_deploy:
+# Fabric script for distributing an archive to web servers.
 
-from fabric.api import run, put, env, local
-import os
-from datetime import datetime
+import os.path
+from fabric.api import env, put, run
 
+# Set the list of remote hosts to distribute the archive to.
 env.hosts = ['54.236.17.124', '100.24.237.123']
 
-# Update with your username
-env.user = "ubuntu"
-
-
-def do_pack():
-    """
-    Function to create a .tgz archive.
-    """
-    try:
-        local("mkdir -p versions")
-        d_t = datetime.now().strftime("%Y%m%d%H%M%S")
-        compr_file = "versions/web_static_{}.tgz".format(d_t)
-        local("tar -cvzf {} web_static".format(compr_file))
-        return compr_file
-    except:
-        return None
 
 def do_deploy(archive_path):
     """
-    Function that distributes an archive.
+    Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+
+    Returns:
+        bool: True if the archive is distributed successfully, False otherwise.
     """
-    name_dir = archive_path[9:-4]
-    if os.path.exists(archive_path):
-        
-        # Upload archive to /tmp/ directory of web server
-        put(archive_path, '/tmp/')
-        
-        # Uncompress archive to /data/web_static/releases/
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(name_dir))
-        run("sudo tar -xzf /tmp/{}.tgz -C \
-        /data/web_static/releases/{}/".format(name_dir, name_dir))
-        run("sudo rm /tmp/{}.tgz".format(name_dir))
-        run("sudo mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".
-            format(name_dir, name_dir))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
-            .format(name_dir))
-        
-        # Delete symbolic link
-        run("sudo rm -rf /data/web_static/current")
-        
-        # Create new symbolic link
-        run("sudo ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current".format(name_dir))
-        print("New version deployed!")
-        return True
-    else:
+
+    # Check if the file exists at archive_path.
+    if os.path.isfile(archive_path) is False:
         return False
+
+    # Extract the name of the file and the name of the directory to create.
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    # Upload the archive to the remote host.
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+
+    # Remove the old directory and create a new one to extract the archive into.
+    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed is True:
+        return False
+
+    # Extract the archive into the new directory.
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name)).failed is True:
+        return False
+
+    # Remove the uploaded archive.
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+
+    # Move the contents of the extracted directory to the new directory.
+    if run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+
+    # Remove the old directory and create a symbolic link to the new directory.
+    if run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)).failed is True:
+        return False
+
+    # Return True if the archive is successfully distributed.
+    return True
