e69cfb4d6f41b29f550e4283af114db48e585e08
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-20 15:30:13 +0300

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,3 +1,23 @@
+#!/usr/bin/python3
+"""Defines unnittests for models/engine/file_storage.py."""
+import os
+import json
+import pep8
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Unittests for testing the FileStorage class."""
+    
 @classmethod
 def setUpClass(cls):
     """FileStorage testing setup.
